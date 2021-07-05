b84ce31c6cfdb673a08c976f098e65c7e1dd2313
yilmaz <bingolyilmaz@hotmail.com>
2021-07-05 04:16:37 -0400

    client updated

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -1,34 +1,8 @@
-// import axios from "axios";
-// //  in incoming request we care about headers
-
-// const baseUrl =
-//   process.env.NODE_ENV === "development"
-//     ? // const baseUrl = (process.env["NODE_ENV"] = "development"
-//       "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local"
-//     : "http://www.ticketing-bingology.xyz/";
-
-// const buildClient = ({ req }) => {
-//   // on server we need to access to headers
-//   if (typeof window === "undefined") {
-//     return axios.create({
-//       // kubectl get namespaces ==> ingress-nginx
-//       //kubectl get services -n ingress-nginx ==> ingress-nginx-controller
-//       // we have to speocify the domain. ingress-nginx will not assume that we want to use the only domain inside the rules
-
-//       baseURL: baseUrl,
-
-//       headers: req.headers,
-//     });
-//   } else {
-//     // in browser, browser takes care of the headers
-//     return axios.create({
-//       baseURL: "/",
-//     });
-//   }
-// };
-// export default buildClient;
-
 import axios from "axios";
+
+export default ({ req }) => {
+  if (typeof window === "undefined") {
+    // We are on the server
 
     return axios.create({
       baseURL:
